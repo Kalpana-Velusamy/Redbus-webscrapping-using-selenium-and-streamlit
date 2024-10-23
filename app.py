4db8449424d76eacef53d15fc8f5b507542c8bce
Kalpana-Velusamy <kalpanavelusamy29@gmail.com>
2024-10-23 05:44:44 +0530

    app.py modified

--- app.py
+++ app.py
@@ -83,13 +83,13 @@ if web=="Home":
     slt.subheader(":red[Ojective:] ")
     slt.markdown("The 'Redbus Data Scraping and Filtering with Streamlit Application' aims to revolutionize the transportation industry by providing a comprehensive solution for collecting, analyzing, and visualizing bus travel data. By utilizing Selenium for web scraping, this project automates the extraction of detailed information from Redbus, including bus routes, schedules, prices, and seat availability. By streamlining data collection and providing powerful tools for data-driven decision-making, this project can significantly improve operational efficiency and strategic planning in the transportation industry.")
     slt.subheader(":red[Overview:]")
-    slt.markdown("Selenium: Selenium is a tool used for automating web browsers. It is commonly used for web scraping, which involves extracting data from websites. Selenium allows you to simulate human interactions with a web page, such as clicking buttons, filling out forms, and navigating through pages, to collect the desired data...")
+    slt.markdown("Selenium: Selenium is a tool used for automating web browsers. It is commonly used for web scraping, which involves extracting data from websites. Selenium allows you to simulate human interactions with a web page, such as clicking buttons, filling out forms, and navigating through pages, to collect the desired data.")
     slt.markdown('''Pandas: Use the powerful Pandas library to transform the dataset from CSV format into a structured dataframe.
                     Pandas helps data manipulation, cleaning, and preprocessing, ensuring that data was ready for analysis.''')
     slt.markdown('''MySQL: With help of SQL to establish a connection to a SQL database, enabling seamless integration of the transformed dataset
                     and the data was efficiently inserted into relevant tables for storage and retrieval.''')
     slt.markdown("Streamlit: Developed an interactive web application using Streamlit, a user-friendly framework for data visualization and analysis.")
-    slt.subheader(":red[Skill-take:]")
+    slt.subheader(":red[Coding Done Using:]")
     slt.markdown("Selenium, Python, Pandas, MySQL,mysql-connector-python, Streamlit.")
     slt.subheader(":red[Developed-by:]  Kalpana Velusamy")
 
